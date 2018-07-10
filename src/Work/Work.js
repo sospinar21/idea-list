ea9df627ddd6893ebf29ac4f7215deb3a63ac58a
Tyler McNamara <mcnamara14@gmail.com>
2018-07-10 16:58:24 -0600

    Add project images

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -28,49 +28,35 @@ class Main extends Component {
     return (
       <div className="work-container" >
         <h1 className='work-title'>Our Latest Work</h1>
+        <p class="work-subtitle">Cras egestas varius posuere. Pellentesque hendrerit tempor auctor. Sed lacinia efficitur ornare. </p>
         <section className='projects'>
-          <article className='project' onClick={this.displayProject}>
-            <div className='photo'></div>
-            <div className='project-text'>
+          <article className='mainProject'>
+            <div className='mainProjectText'>
               <h2>DayTrip</h2>
-              <p>Web application</p>
+              <p>Web Application ></p>
             </div>
           </article>
-          <article className='project' onClick={this.displayProject}>
-            <div className='photo'></div>
+          <article className='project project-1' onClick={this.displayProject}>
             <div className='project-text'>
-              <h2>DayTrip</h2>
-              <p>Web application</p>
+              <h2>Evo-Lite</h2>
+              <p>Website / Branding / Online Marketing</p>
+              <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
             </div>
           </article>
-          <article className='project' onClick={this.displayProject}>
-            <div className='photo'></div>
+          <article className='project project-2' onClick={this.displayProject}>
             <div className='project-text'>
-              <h2>DayTrip</h2>
-              <p>Web application</p>
+              <h2>Viridis Design Studio</h2>
+              <p>CMS Website</p>
+              <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
             </div>
           </article>
-          <article className='project' onClick={this.displayProject}>
-            <div className='photo'></div>
+          <article className='project  project-3' onClick={this.displayProject}>
             <div className='project-text'>
-              <h2>DayTrip</h2>
+              <h2>AWC</h2>
               <p>Web application</p>
+              <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
             </div>
-          </article>
-          <article className='project' onClick={this.displayProject}>
-            <div className='photo'></div>
-            <div className='project-text'>
-              <h2>DayTrip</h2>
-              <p>Web application</p>
-            </div>
-          </article>
-          <article className='project' onClick={this.displayProject}>
-            <div className='photo'></div>
-            <div className='project-text'>
-              <h2>DayTrip</h2>
-              <p>Web application</p>
-            </div>
-          </article>          
+          </article>        
         </section>
       </div>
     );
